f71a4ff164ba0ee48d5bb29f2d75a4870f24ad96
rwolf <rwolf@cisco.com>
2019-10-11 17:54:31 -0700

    Blog functionality and polish

--- blog/models.py
+++ blog/models.py
@@ -8,3 +8,16 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        len_limit = 150
+        suffix = ''
+        if len(self.body) > len_limit:
+            suffix = ' ...'
+        return self.body[:len_limit] + suffix
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
